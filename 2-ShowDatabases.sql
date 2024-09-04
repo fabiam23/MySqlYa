71d91bf33aee082dc55f83c8dbae4c2f1efb54d2
FabiamWork <fabiam23@gmail.com>
2024-09-04 11:52:32 -0400

    Update 2-ShowDatabases.sql

--- 2-ShowDatabases.sql
+++ 2-ShowDatabases.sql
@@ -1,5 +1,7 @@
 # Una base de datos es un conjunto de tablas
 
 # El siguiente comando muestra los nombrres de todas las bases de datos
-
 show databases;
+
+# create database crea una nueva base de datos
+create database lmg;
